911322a609e6dbfbdfe84932289e198e40e5e3b6
okemwag <gabrielokemwa02@gmail.com>
2025-05-20 14:33:12 +0300

    chore: Fix Cors Issues

--- assets/models.py
+++ assets/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils import timezone
+from decimal import Decimal
 
 User = get_user_model()
 
