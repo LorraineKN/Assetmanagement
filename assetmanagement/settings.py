be4876f4930511ceccdfeb1db728ba5fe32e568b
okemwag <gabrielokemwa02@gmail.com>
2025-05-20 13:21:46 +0300

    chore: Fix Cors Issues

--- assetmanagement/settings.py
+++ assetmanagement/settings.py
@@ -31,11 +31,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "users",
     "assets",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
